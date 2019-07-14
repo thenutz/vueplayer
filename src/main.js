2ae14b1478b57650d45b465301ba64e526d1739f
thenutz <thenutz@thenutz.ro>
2019-07-14 20:39:00 +0300

    overflow fixed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import vueDebounce from "vue-debounce";
 Vue.config.productionTip = false;
-
+Vue.use(vueDebounce);
 new Vue({
   render: h => h(App)
 }).$mount("#app");
